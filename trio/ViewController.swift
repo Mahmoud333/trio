1b74f0aa47bc551ef870f394e1ce36b06e381924
mahmoud epic <mahmoud_smgl@epicsyst.net>
2018-02-11 15:04:22 +0200

    Initial Commit

---
+++ trio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  trio
+//
+//  Created by mac on 2/11/18.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
